60ca6a1e2c626a38a8265f19546fbc0f912413b2
Antonio Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2021-06-03 20:25:16 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Servidor começou na porta 3333');
 });
